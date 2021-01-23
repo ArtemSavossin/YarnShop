6986dcbcbcd25c1153de76635f99c443b91c8403
Artyom <savossinartem@gmail.com>
2021-01-23 12:19:11 +0300

    gzip on backend

--- yarn-back/server.js
+++ yarn-back/server.js
@@ -1,56 +1,58 @@
-import path from "path"
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import uploadRoutes from "./routes/uploadRoutes.js"
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+const __dirname = path.resolve();
 
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/upload", uploadRoutes)
+app.use(express.json());
 
-const __dirname = path.resolve()
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/yarn-front/build")))
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "yarn-front", "build", "index.html"))
-  )
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(expressStaticGzip(path.join(__dirname, 'build'), {}));
+  app.use(express.static(path.join(__dirname, '/yarn-front/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'yarn-front', 'build', 'index.html'))
+  );
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running")
-  })
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
-})
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
 app.use((err, req, res, next) => {
-  const error = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(error)
+  const error = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(error);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  })
-})
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`running in ${process.env.NODE_ENV} on port ${PORT}`)
-)
+);
