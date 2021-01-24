3fbb63a4c643ec2d922c2b75bfa647a887dcff14
Artyom <savossinartem@gmail.com>
2021-01-24 12:55:56 +0300

    footer added contats template

--- yarn-front/src/components/Footer.js
+++ yarn-front/src/components/Footer.js
@@ -6,18 +6,20 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col xs={4}>
-            <Row>
+          <Col xs={12} sm={4} className='text-center py-3'>
+            <Row className='text-center py-3'>
               <Col xs={12}>Miss Yarn в соцсетях:</Col>
               <Col xs={12}>Instagram</Col>
               <Col xs={12}>VK</Col>
             </Row>
           </Col>
-          <Col xs={4} className='text-center py-3'>
-            Техподдержка
+          <Col xs={12} sm={4} className='text-center py-3'>
+            <Row className='text-center py-3'>
+              <Col>Техподдержка</Col>
+            </Row>
           </Col>
-          <Col xs={4}>
-            <Row>
+          <Col xs={12} sm={4} className='text-center py-3'>
+            <Row className='text-center py-3'>
               <Col xs={12}>Сотрудничество с разработчиками</Col>
               <Col xs={12}>Дизайнер</Col>
               <Col xs={12}>Разаработчик сайта</Col>
